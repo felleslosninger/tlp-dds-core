0518fe18f1cce9e054758aa0328526e9e18a15ad
Bendik Kvernevik <bendiksem93@gmail.com>
2021-11-15 18:00:06 +0100

    docs: Added "Typografi" story

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,14 @@
 import customTheme from './customTheme.js'
+// Import assets available to all stories
+import '../packages/css/src/index.scss'
+import '../packages/components/src/index.js'
 
+// Parsing configuration for design-token storybook addon
 const tokenContext = require.context(
   '!!raw-loader!../packages/tokens/src/stories',
   true,
   /.\.(css|less|scss|svg)$/,
 )
-
 const tokenFiles = tokenContext.keys().map(function (filename) {
   return { filename: filename, content: tokenContext(filename).default }
 })
@@ -26,7 +29,7 @@ export const parameters = {
       order: [
         'Introduksjon',
         'Designsystem',
-        ['Tokens', 'Komponenter', 'Maler', ['Introduksjon']],
+        ['Tokens', 'Typografi', 'Komponenter', 'Maler', ['Introduksjon']],
       ],
     },
   },
