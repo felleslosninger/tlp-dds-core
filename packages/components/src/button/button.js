f08086058a689472dcf007afc2596aa775897de5
Bendik Kvernevik <bendiksem93@gmail.com>
2021-11-19 13:53:59 +0100

    feat: Added button HTML templating function

---
+++ packages/components/src/button/button.js
@@ -0,0 +1,52 @@
+import classnames from 'classnames'
+
+const createButton = ({
+  label = 'Click me',
+  variant = '',
+  disabled = false,
+  size = 'medium',
+  iconLeft = false,
+  iconRight = false,
+  iconSvgPathLeft = String.raw`<path d="M19.51 3.08 3.08 19.51c.09.34.27.65.51.9.25.24.56.42.9.51L20.93 4.49c-.19-.69-.73-1.23-1.42-1.41zM11.88 3 3 11.88v2.83L14.71 3h-2.83zM5 3c-1.1 0-2 .9-2 2v2l4-4H5zm14 18c.55 0 1.05-.22 1.41-.59.37-.36.59-.86.59-1.41v-2l-4 4h2zm-9.71 0h2.83L21 12.12V9.29L9.29 21z"></path>`,
+  iconSvgPathRight = String.raw`<path d="M19.51 3.08 3.08 19.51c.09.34.27.65.51.9.25.24.56.42.9.51L20.93 4.49c-.19-.69-.73-1.23-1.42-1.41zM11.88 3 3 11.88v2.83L14.71 3h-2.83zM5 3c-1.1 0-2 .9-2 2v2l4-4H5zm14 18c.55 0 1.05-.22 1.41-.59.37-.36.59-.86.59-1.41v-2l-4 4h2zm-9.71 0h2.83L21 12.12V9.29L9.29 21z"></path>`,
+}) => {
+  const classPrefix = 'dads-button'
+  const buttonClasses = classnames(
+    classPrefix,
+    { [`${classPrefix}--secondary`]: variant === 'secondary' },
+    { [`${classPrefix}--x-small`]: size === 'x-small' },
+    { [`${classPrefix}--small`]: size === 'small' },
+    { [`${classPrefix}--large`]: size === 'large' },
+    { [`${classPrefix}--x-large`]: size === 'x-large' },
+  )
+  const iconSvgLeft = String.raw`<svg
+    class="${classPrefix}__icon-left"
+    focusable="false"
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    >
+      ${iconSvgPathLeft}
+    </svg>`
+
+  const iconSvgRight = String.raw`<svg
+    class="${classPrefix}__icon-right"
+    focusable="false"
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    >
+      ${iconSvgPathRight}
+    </svg>`
+
+  return String.raw`
+  <button
+    type='button'
+    class='${buttonClasses}'
+    ${disabled ? 'disabled' : ''}
+  >
+    ${iconLeft ? iconSvgLeft : ''}
+    ${label}
+    ${iconRight ? iconSvgRight : ''}
+  </button>`
+}
+
+export { createButton }
