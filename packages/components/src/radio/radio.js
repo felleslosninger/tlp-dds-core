1116b218847c4dd945b7285de57fcde6777ca72a
Bendik Kvernevik <bendiksem93@gmail.com>
2021-11-25 11:03:52 +0100

    feat: Added radio button HTML templating

---
+++ packages/components/src/radio/radio.js
@@ -0,0 +1,28 @@
+import classnames from 'classnames'
+import { sanitizeStrict, formatHtml } from '../utils'
+
+const createRadio = ({
+  label = 'Click me',
+  size = 'medium',
+  disabled = false,
+  checked = false,
+  required = false,
+}) => {
+  label = sanitizeStrict(label)
+  const classPrefix = 'dads-radio'
+  const customRadioClasses = classnames([`${classPrefix}`], {
+    [`${classPrefix}--large`]: size === 'large',
+    [`${classPrefix}--disabled`]: disabled,
+  })
+  return formatHtml(String.raw`
+    <label class='${customRadioClasses}'>
+      <input type='radio'
+        ${disabled ? 'disabled' : ''} 
+        ${checked ? 'checked' : ''}
+        ${required ? 'required' : ''}
+      />
+      ${label}
+    </label>`)
+}
+
+export { createRadio }
