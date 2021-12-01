b7a5c5bc7621faf749dba45c56911decb441dda3
Bendik Kvernevik <bendiksem93@gmail.com>
2021-12-01 13:35:51 +0100

    WIP: layout

--- packages/templates/src/baseLayout.js
+++ packages/templates/src/baseLayout.js
@@ -1,45 +1,37 @@
-const createBaseLayout = ({
-  title = 'DigDir title',
-  content = '',
-  stylePath = '../../css/build/index.css',
-  scriptPathEs5 = '../../components/build/index.cjs',
-  scriptPathEs6 = '../../components/build/index.js',
-}) => {
+const createBaseLayout = ({ content = '' }) => {
   return String.raw`
-<!DOCTYPE html>
-<html lang="no">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <div class="dads-topbar">
+  </div>
 
-    <!-- DigDir admin design system assets -->
-    <link rel="stylesheet" href="${stylePath}" />
-    <!-- ES6 script -->
-    <script type="module" src="${scriptPathEs6}"></script>
-    <!-- ES5 fallback script -->
-    <script nomodule src="${scriptPathEs5}" defer></script>
+  <div class="dads-sidebar">
+    <div class="dads-sidebar__header">
 
-    <title>${title} | DigDir admin </title>
-  </head>
-  <body>
-    <div class="page">
-      <div class="top-bar"></div>
-
-      <div class="sidebar">
-        <nav>
-          <ul class="menu">
-            <li class="menu__item"></li>
-          </ul>
-        </nav>
+      <div class="dads-sidebar__header__logo">
+        <svg class="dads-sidebar__header__logo__icon" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg">
+          <path d="M12.478 0H0.492424C0.362615 0.00252128 0.238824 0.0552112 0.147018 0.147018C0.0552112 0.238824 0.00252128 0.362615 0 0.492424V23.8727C0.00252128 24.0025 0.0552112 24.1263 0.147018 24.2181C0.238824 24.3099 0.362615 24.3626 0.492424 24.3652H12.478C15.709 24.3652 18.8077 23.0816 21.0924 20.797C23.3771 18.5123 24.6606 15.4136 24.6606 12.1826C24.6606 8.95156 23.3771 5.85287 21.0924 3.56819C18.8077 1.28352 15.709 4.81459e-08 12.478 0ZM13.65 20.6818C11.8366 20.9939 9.97084 20.7207 8.32302 19.9018C6.67519 19.0828 5.33079 17.7607 4.48448 16.1267C3.63817 14.4928 3.33387 12.6319 3.61566 10.8135C3.89745 8.99512 4.7507 7.3136 6.05185 6.01245C7.353 4.7113 9.03451 3.85806 10.8529 3.57627C12.6713 3.29448 14.5322 3.59877 16.1661 4.44508C17.8001 5.2914 19.1222 6.6358 19.9412 8.28363C20.7601 9.93145 21.0333 11.7972 20.7212 13.6106C20.4175 15.3754 19.5746 17.0027 18.3084 18.269C17.0421 19.5352 15.4148 20.3781 13.65 20.6818"/>
+        </svg>
+        
+        <span class="dads-sidebar__header__logo__label">
+          Digdir
+        </span>
       </div>
 
-      <main class="content">
-        ${content}
-      </main>
+
+
+      <div class="dads-sidebar__header__handle">
+      <button class="dads-icon-button">
+        <svg class="dads-icon-button__icon" viewBox="0 0 88 75" xmlns="http://www.w3.org/2000/svg">
+          <path d="M57.8125 7.03125C56.8359 6.05469 55.2734 6.05469 54.4922 7.03125L50.5859 10.9375C49.6094 11.7188 49.6094 13.2812 50.5859 14.2578L69.5312 32.6172H21.0938C19.7266 32.6172 18.75 33.5938 18.75 34.9609V40.4297C18.75 41.6016 19.7266 42.7734 21.0938 42.7734H69.5312L50.5859 60.9375C49.6094 61.9141 49.6094 63.4766 50.5859 64.2578L54.4922 68.1641C55.2734 69.1406 56.8359 69.1406 57.8125 68.1641L86.7188 39.2578C87.6953 38.2812 87.6953 36.9141 86.7188 35.9375L57.8125 7.03125ZM10.1562 72.6562V2.34375C10.1562 1.17188 8.98438 0 7.8125 0H2.34375C0.976562 0 0 1.17188 0 2.34375V72.6562C0 74.0234 0.976562 75 2.34375 75H7.8125C8.98438 75 10.1562 74.0234 10.1562 72.6562Z"></path>
+        </svg>
+      </button>
+      </div>
+
     </div>
-  </body>
-</html>
-`
+  </div>
+
+  <main class="dads-content-container">
+    ${content}
+  </main>`
 }
 
 export { createBaseLayout }
--- packages/templates/stories/baseLayout.stories.js
+++ packages/templates/stories/baseLayout.stories.js
@@ -1,5 +1,42 @@
-import '../../components/build/index.css'
 import { createBaseLayout } from '../src/baseLayout.js'
 
-// Export story for baseLayout
-// ...
+export default {
+  title: 'Admin/Maler',
+  parameters: {
+    layout: 'fullscreen',
+    docs: {
+      inlineStories: false,
+    },
+    controls: {
+      disabled: true,
+    },
+  },
+}
+
+const baseLayout = createBaseLayout({
+  content: String.raw`
+  <h1 class='dads-text-heading-900'>Digdir</h1>
+  <p class='dads-text-ingress-600'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nulla ex, tempus ac faucibus at, faucibus ut dolor. Etiam placerat massa arcu, et mollis libero dignissim sed. Nullam massa nibh, interdum a lorem vitae, pharetra scelerisque dui. </p>
+
+  <p class='dads-text-body-400'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nulla ex, tempus ac faucibus at, faucibus ut dolor. Etiam placerat massa arcu, et mollis libero dignissim sed. Nullam massa nibh, interdum a lorem vitae, pharetra scelerisque dui. 
+    Vestibulum hendrerit magna finibus ante pulvinar egestas. Curabitur venenatis erat at purus pharetra accumsan. Suspendisse id egestas metus. Proin laoreet cursus massa id varius. Pellentesque dignissim ipsum leo, non malesuada risus mattis vitae. Pellentesque pharetra lacus nec urna commodo consequat.
+    Nam convallis nunc ac ornare facilisis. Ut nunc velit, tristique eget augue vel, facilisis luctus nulla. Nulla iaculis, orci quis tincidunt luctus, lacus orci imperdiet tellus, vel laoreet ligula ante et dolor. Vivamus maximus venenatis purus, nec sollicitudin justo molestie a. 
+    Nunc magna quam, condimentum ac convallis id, pulvinar ac sapien. 
+  </p>
+  <p class='dads-text-body-400'>
+    In venenatis fermentum libero, sed venenatis ex ultricies eu. Vestibulum sit amet lacus eu ligula vulputate varius. Ut sit amet leo sed orci scelerisque ornare feugiat sit amet erat. Quisque tempor a augue nec placerat. Etiam dapibus semper risus, eu viverra tellus consectetur ac. Suspendisse in magna eu ligula sagittis tristique vel vitae nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla egestas tempus scelerisque.
+    Suspendisse potenti. Donec malesuada, nisi eget pellentesque mollis, nunc nunc vestibulum nulla, ut volutpat tortor lorem sed tortor. Curabitur eget lacus magna. Donec sed tellus quis felis semper gravida. Sed tristique rutrum lobortis. Maecenas quis augue quis risus facilisis pulvinar et ut odio. 
+    Donec sollicitudin fringilla facilisis. Maecenas eu neque libero. Duis id felis ipsum. Vivamus fringilla id nisl eu blandit. Nulla luctus, justo at lacinia feugiat, lorem ligula aliquet metus, ut maximus orci nisi faucibus odio. Vestibulum vitae semper orci, quis blandit leo. 
+  </p>
+  <p class='dads-text-body-200'></p>
+    Phasellus sed lobortis quam.
+    Aliquam erat volutpat. Integer tortor leo, porta et arcu quis, eleifend dignissim elit. Nam magna ligula, interdum vel dapibus sit amet, tempus at urna. Sed sodales euismod purus, at rhoncus quam scelerisque sed. Nunc vitae felis sollicitudin, ultrices nisi pellentesque, imperdiet quam. 
+    Donec consectetur eget dolor quis gravida. Nulla non dui eu sem tincidunt luctus id sed libero. Fusce pellentesque pulvinar dolor nec facilisis. Aliquam sed sapien eu leo luctus convallis non id odio. Aliquam dignissim purus eget libero ultricies gravida id non neque. In hac habitasse platea dictumst. Quisque nec ligula pulvinar, viverra quam vel, gravida purus. Suspendisse a mi massa. Maecenas ultrices lorem lectus, semper sollicitudin libero consectetur vitae. Proin venenatis gravida eros, at facilisis orci blandit finibus.
+    Suspendisse mollis metus ut molestie laoreet. Maecenas egestas convallis velit, nec tristique tortor volutpat rhoncus. Nam ac augue aliquam risus sodales eleifend. Nunc auctor nulla nec ipsum lacinia vehicula. In hac habitasse platea dictumst. Cras sagittis rhoncus volutpat. Vivamus vitae ullamcorper nunc, id venenatis nulla. Morbi vel pretium elit. Praesent sit amet tortor libero. In aliquet libero imperdiet odio convallis hendrerit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+  </p>
+  `,
+})
+
+export const Layout = () => {
+  return baseLayout
+}
