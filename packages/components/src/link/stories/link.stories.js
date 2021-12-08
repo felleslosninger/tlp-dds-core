a06c4a85798637df6c62f0b6b033b3486c1c5fec
Bendik Kvernevik <bendiksem93@gmail.com>
2021-12-08 12:37:05 +0100

    docs: add icon link stories
    * story for internal and external link

---
+++ packages/components/src/link/stories/link.stories.js
@@ -0,0 +1,56 @@
+import { createLink } from '../link'
+import { withDesign } from 'storybook-addon-designs'
+
+export default {
+  title: 'Admin/Komponenter/Icon-link',
+  decorators: [withDesign],
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/vpM9dqqQPHqU6ogfKp5tlr/?node-id=2365%3A23464',
+    },
+  },
+  argTypes: {
+    label: {},
+    size: {
+      options: ['medium', 'large'],
+      control: { type: 'select' },
+    },
+    variant: {
+      options: ['internal', 'external'],
+      control: { type: 'select' },
+    },
+  },
+}
+
+const Template = (args) => {
+  return createLink(args)
+}
+
+export const InternalLink = Template.bind({})
+InternalLink.args = {
+  label: 'Internal link',
+  size: 'medium',
+  variant: 'internal',
+}
+InternalLink.parameters = {
+  docs: {
+    source: {
+      code: Template(InternalLink.args),
+    },
+  },
+}
+
+export const ExternalLink = Template.bind({})
+ExternalLink.args = {
+  label: 'External link',
+  size: 'medium',
+  variant: 'external',
+}
+ExternalLink.parameters = {
+  docs: {
+    source: {
+      code: Template(ExternalLink.args),
+    },
+  },
+}
