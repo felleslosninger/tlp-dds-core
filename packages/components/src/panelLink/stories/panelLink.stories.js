9c650bf81099eb532a4ef2c08b4cd345d611baf7
Bendik Kvernevik <bendiksem93@gmail.com>
2021-12-13 16:28:58 +0100

    docs(storybook): add panel link story to SB

---
+++ packages/components/src/panelLink/stories/panelLink.stories.js
@@ -0,0 +1,37 @@
+import { createPanelLink } from '../panelLink'
+import { withDesign } from 'storybook-addon-designs'
+
+export default {
+  title: 'Komponenter/Panel-link',
+  decorators: [withDesign],
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/vpM9dqqQPHqU6ogfKp5tlr/?node-id=2365%3A23464',
+    },
+  },
+  argTypes: {
+    label: {},
+    size: {
+      options: ['medium', 'large'],
+      control: { type: 'select' },
+    },
+  },
+}
+
+const Template = (args) => {
+  return createPanelLink(args)
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  label: 'Panel link',
+  size: 'medium',
+}
+Default.parameters = {
+  docs: {
+    source: {
+      code: Template(Default.args),
+    },
+  },
+}
