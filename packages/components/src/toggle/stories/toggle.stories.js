89e54e93889056d5aef51722a7741c8a10ff91cc
Bendik Kvernevik <bendiksem93@gmail.com>
2022-01-12 15:30:46 +0100

    docs(storybook): add Toggle stories
    * left label
    * dual label

--- packages/components/src/toggle/stories/toggle.stories.js
+++ packages/components/src/toggle/stories/toggle.stories.js
@@ -13,6 +13,11 @@ export default {
   argTypes: {
     label: {},
     checked: {},
+    variant: {
+      options: ['default', 'leftLabel', 'dualLabel'],
+      control: { type: 'select' },
+    },
+    toggleLabel: {},
   },
 }
 
@@ -24,4 +29,22 @@ export const Default = Template.bind({})
 Default.args = {
   label: 'Bryter',
   checked: false,
+  variant: 'default',
+  toggleLabel: true,
+}
+
+export const DualLabel = Template.bind({})
+DualLabel.args = {
+  label: 'Bryter',
+  checked: false,
+  variant: 'dualLabel',
+  toggleLabel: true,
+}
+
+export const LeftLabel = Template.bind({})
+LeftLabel.args = {
+  label: 'Bryter',
+  checked: false,
+  variant: 'leftLabel',
+  toggleLabel: false,
 }
