96328bb3d419791a8c935c97444ca0b1e0ad8c14
Bendik Kvernevik <bendiksem93@gmail.com>
2022-01-12 15:29:31 +0100

    feat(toggle): add variants: left label and dual label
    Feats:
    Add support-classes for having label at the left side, and having two labels - one on each side - describing the two states that are toggled between. The labels may be "faded" in/out, in sync with the toggle state, by using additional utility classes (.dds-toggle__label--toggle-on/off).
    
    Using multiple labels is challenging due to different behaviour of screen readers. Thus, the aria-labelledby attribute should be used when using more than one label (see examples in docs).

--- packages/components/src/toggle/toggle.js
+++ packages/components/src/toggle/toggle.js
@@ -3,25 +3,62 @@ import { sanitizeStrict, formatHtml } from '../utils.js'
 
 const createToggle = ({
   label = 'Click me',
-  inputElementId = 'toggle-input-field-id',
+  inputElementId = 'toggle-input-id',
   size = 'medium',
   checked = false,
+  toggleLabel = true,
+  variant = 'default', // default | leftLabel | dualLabel
 }) => {
+  const dualLabel = variant === 'dualLabel'
   label = sanitizeStrict(label)
+
   const classPrefix = 'dds-toggle'
   const customToggleClasses = classnames([`${classPrefix}`], {
     [`${classPrefix}--small`]: size === 'small',
   })
+  const labelClassPrefix = `${classPrefix}__label`
+  const customLabelClasses = classnames(
+    [`${labelClassPrefix}`],
+    {
+      [`${labelClassPrefix}--toggle-on`]: toggleLabel,
+    },
+    { [`${labelClassPrefix}--right`]: variant === 'default' },
+    { [`${labelClassPrefix}--left`]: variant === 'leftLabel' },
+  )
+
+  const labelledToggleElement = (mainLabel) =>
+    `<label class='${customLabelClasses}' for=${inputElementId}>${mainLabel}</label>`
+
+  // Toggle with single label
+  if (!dualLabel) {
+    return formatHtml(String.raw`
+      <div class='${customToggleClasses}'>
+        <input class='${classPrefix}__input' type='checkbox' id=${inputElementId}
+        ${checked ? 'checked' : ''}
+        />
+        ${labelledToggleElement(label)}
+      </div>`)
+  }
+
+  // Toggle with two labels
+  const labelLeftId = 'toggle-label-left'
+  const labelRightId = 'toggle-label-right'
+  const labelLeftElement = `<label class='${classPrefix}__decoupled-label-left ${
+    toggleLabel ? classPrefix + '__label--toggle-off' : ''
+  }' id='${labelLeftId}'> Av </label>`
+  const labelRightElement = `<label class='${classPrefix}__decoupled-label-right ${
+    toggleLabel ? classPrefix + '__label--toggle-on' : ''
+  }' id='${labelRightId}'> På </label>`
 
   return formatHtml(String.raw`
-  <div class='${customToggleClasses}'>
-    <input class='${classPrefix}__input' type='checkbox' id=${inputElementId}
-      ${checked ? 'checked' : ''}
-    />
-    <label class='${classPrefix}__label' for=${inputElementId}>
-      ${label}
-    </label>
-  </div>`)
+    <div class='${customToggleClasses}'>
+      <input class='${classPrefix}__input' type='checkbox' id=${inputElementId} aria-labelledby='${labelLeftId} ${labelRightId}'
+        ${checked ? 'checked' : ''}
+      />
+      ${dualLabel ? labelLeftElement : ''}
+      ${labelledToggleElement('')}
+      ${dualLabel ? labelRightElement : ''}
+    </div>`)
 }
 
 export { createToggle }
