de346564b3f5e2deb0c595f89bd9e7d279588c4a
Bendik Kvernevik <bendiksem93@gmail.com>
2022-01-18 16:22:44 +0100

    docs(storybook): remove ID-porten integration
    ID-porten Storybook integration is no longer relevant in revised project goals. Other integrations will be added as needed.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,20 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  stories: ['../.storybook/**/*.stories.@(js|jsx|ts|tsx|mdx)', '../packages/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-scss', '@storybook/theming', '@whitespace/storybook-addon-html', '@storybook/addon-a11y', 'storybook-addon-designs'],
-  refs: {
-    // ID-porten storybook integration
-    idporten: {
-      title: 'ID-porten',
-      url: 'https://felleslosninger.github.io/idporten-design-system/',
-      expanded: false
-    }
-  },
+  stories: [
+    '../.storybook/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    '../packages/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-scss',
+    '@storybook/theming',
+    '@whitespace/storybook-addon-html',
+    '@storybook/addon-a11y',
+    'storybook-addon-designs',
+  ],
   core: {
-    builder: "webpack5"
-  }
-};
+    builder: 'webpack5',
+  },
+}
