64eacbc440fb170baa5e91411fe2e08aef1bc14d
Bendik Kvernevik <bendiksem93@gmail.com>
2022-01-25 11:56:38 +0100

    docs(storybook): update javascript info in radio/checkbox
    Use info box from design for javascript information in radio/checkbox.

--- packages/html/src/checkbox/stories/checkbox.stories.js
+++ packages/html/src/checkbox/stories/checkbox.stories.js
@@ -2,16 +2,7 @@ import { createCheckbox } from '../checkbox.js'
 import { checkboxScript } from '../../../../js/src/checkbox.script.js'
 import { withDesign } from 'storybook-addon-designs'
 import { useEffect } from '@storybook/client-api'
-
-const componentDescription = String.raw`
-<div style="display: flex; align-items: center;">
-  <div style="border-radius: 4px; border: solid 2px; margin-right: 10px;padding: 4px;">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32px" height="32px"  fill="#ffc922"><path d="M11 15h2v2h-2v-2zm0-8h2v6h-2V7zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/></svg>
-    <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" width="32px" height="32px" viewBox="0 0 24 24" fill="#000000"><g><rect fill="none" height="24" width="24"/></g><g><path d="M12,14v-1h1.5v0.5h2v-1H13c-0.55,0-1-0.45-1-1V10c0-0.55,0.45-1,1-1h3c0.55,0,1,0.45,1,1v1h-1.5v-0.5h-2v1H16 c0.55,0,1,0.45,1,1V14c0,0.55-0.45,1-1,1h-3C12.45,15,12,14.55,12,14z M9,9v4.5H7.5v-1H6v1C6,14.33,6.67,15,7.5,15H9 c0.83,0,1.5-0.67,1.5-1.5V9C10.5,9,9.83,9,9,9z"/></g></svg>
-  </div>
-  Denne komponenten bruker JavaScript.
-</div>
-`
+import { jsInfo } from '/docs-main/docsComponents.js'
 
 export default {
   title: 'Komponenter/Checkbox',
@@ -23,7 +14,7 @@ export default {
     },
     docs: {
       description: {
-        component: componentDescription,
+        component: jsInfo,
       },
     },
   },
