a56661c7ac8a3c3c6d0f980f63a671d9df6ace44
Bendik Kvernevik <bendiksem93@gmail.com>
2022-01-31 13:50:30 +0100

    docs(storybook): link story: make text-class opt-in control
    The default use case would be to define text-class somewhere else, not on <a> element.

--- packages/html/src/link/stories/link.stories.js
+++ packages/html/src/link/stories/link.stories.js
@@ -35,12 +35,12 @@ export default {
   },
 }
 
-const Template = (args) => {
-  let linkHtml = String.raw`<a class="dds-link ${args.textClass}" href="https://www.digdir.no/" target="_blank" rel="noreferrer noopener">Slik ser en lenke til Digdir ut i løpende tekst</a>`
+const Template = ({ textClass = '', ...args }) => {
+  let linkHtml = String.raw`<a class="dds-link ${textClass}" href="https://www.digdir.no/" target="_blank" rel="noreferrer noopener">Slik ser en lenke til Digdir ut i løpende tekst</a>`
 
   if (args.paragraph) {
     linkHtml = String.raw`
-    <p class="${args.textClass}">
+    <p class="${textClass}">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed dolor mi. Cras quis velit erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempor quam vel elit aliquam, id vulputate neque rutrum. Phasellus vehicula at tellus quis lacinia. Nam nec erat vitae eros ornare elementum id vel quam.
       <a class="dds-link" href="https://www.digdir.no/" target="_blank" rel="noreferrer noopener">Slik ser en lenke til Digdir ut i løpende tekst</a>.
       Donec eget molestie lacus. Nulla ac eros et nisl sodales congue in eget massa. Fusce aliquet est vitae sem eleifend congue. Suspendisse id risus vitae sapien maximus bibendum. Fusce non ipsum ac orci varius sollicitudin a in ante.
@@ -52,6 +52,5 @@ const Template = (args) => {
 
 export const Link = Template.bind({})
 Link.args = {
-  textClass: 'dds-text-body-400',
   paragraph: false,
 }
