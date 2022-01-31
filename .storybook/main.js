ff3896c4596a7cf2cdbe412414e683de92eb6c01
Bendik Kvernevik <bendiksem93@gmail.com>
2022-01-31 14:01:27 +0100

    refactor(storybook): move general stories from config folder .storybook/ to docs-main/stories/

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   stories: [
-    '../.storybook/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    '../docs-main/**/*.stories.@(js|jsx|ts|tsx|mdx)',
     '../packages/**/*.stories.@(js|jsx|ts|tsx|mdx)',
   ],
   addons: [
