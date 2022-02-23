1d5e852fdfbc8a752ba0dc871adcb2771a65a791
Bendik Kvernevik <bendiksem93@gmail.com>
2022-02-23 17:46:11 +0100

    refactor(input-field): use directly imported SVG
    BREAKING CHANGE:
    The change requires use of wrapper span on SVG elements in the input-field component structure. The classes previously used directly on the SVG's (&__icon-left and &__icon-right) are moved to the parent wrapper span.

--- packages/html/src/inputField/inputField.js
+++ packages/html/src/inputField/inputField.js
@@ -1,5 +1,7 @@
 import classnames from 'classnames'
 import { sanitizeStrict, formatHtml } from '../utils.js'
+import imageIcon from '@digdir/ds-icons/svg/image/outline.svg'
+import searchIcon from '@digdir/ds-icons/svg/search/outline.svg'
 
 const createInputField = ({
   label = 'Label',
@@ -34,24 +36,12 @@ const createInputField = ({
     'M19.51 3.08L3.08 19.51c.09.34.27.65.51.9.25.24.56.42.9.51L20.93 4.49c-.19-.69-.73-1.23-1.42-1.41zM11.88 3L3 11.88v2.83L14.71 3h-2.83zM5 3c-1.1 0-2 .9-2 2v2l4-4H5zm14 18c.55 0 1.05-.22 1.41-.59.37-.36.59-.86.59-1.41v-2l-4 4h2zm-9.71 0h2.83L21 12.12V9.29L9.29 21z'
   const iconLeftSvg = String.raw`
     <span class='${classPrefix}__icon-left'>
-      <svg
-        focusable="false"
-        viewBox="0 0 24 24"
-        aria-hidden="true"
-        >
-        <path d="${iconSvgPathTexture}"></path>
-      </svg>
+      ${imageIcon}
     </span>
     `
   const iconRightSvg = String.raw`
     <span class='${classPrefix}__icon-right'>
-      <svg
-        focusable="false"
-        viewBox="0 0 24 24"
-        aria-hidden="true"
-        >
-        <path d="${iconSvgPathSearch}"></path>
-      </svg>
+      ${searchIcon}
     </span>
     `
 
