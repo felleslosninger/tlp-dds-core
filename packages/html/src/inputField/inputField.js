38ee44c8a9ef3b117c7a8488e0c9f90d71caf491
Bendik Kvernevik <bendiksem93@gmail.com>
2022-02-23 16:50:17 +0100

    feat(input-field): remove class from SVG element, introduce wrapper element
    Allows greater developer flexibility. It is now possible to import svg directly into structure without having to manipulate element by adding classes.

--- packages/html/src/inputField/inputField.js
+++ packages/html/src/inputField/inputField.js
@@ -33,23 +33,27 @@ const createInputField = ({
   const iconSvgPathTexture =
     'M19.51 3.08L3.08 19.51c.09.34.27.65.51.9.25.24.56.42.9.51L20.93 4.49c-.19-.69-.73-1.23-1.42-1.41zM11.88 3L3 11.88v2.83L14.71 3h-2.83zM5 3c-1.1 0-2 .9-2 2v2l4-4H5zm14 18c.55 0 1.05-.22 1.41-.59.37-.36.59-.86.59-1.41v-2l-4 4h2zm-9.71 0h2.83L21 12.12V9.29L9.29 21z'
   const iconLeftSvg = String.raw`
-    <svg
-      class="${classPrefix}__icon-left"
-      focusable="false"
-      viewBox="0 0 24 24"
-      aria-hidden="true"
-      >
-      <path d="${iconSvgPathTexture}"></path>
-    </svg>`
+    <span class='${classPrefix}__icon-left'>
+      <svg
+        focusable="false"
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+        >
+        <path d="${iconSvgPathTexture}"></path>
+      </svg>
+    </span>
+    `
   const iconRightSvg = String.raw`
-    <svg
-      class="${classPrefix}__icon-right"
-      focusable="false"
-      viewBox="0 0 24 24"
-      aria-hidden="true"
-      >
-      <path d="${iconSvgPathSearch}"></path>
-    </svg>`
+    <span class='${classPrefix}__icon-right'>
+      <svg
+        focusable="false"
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+        >
+        <path d="${iconSvgPathSearch}"></path>
+      </svg>
+    </span>
+    `
 
   // Attributes
   const ariaLabelAttribute = `aria-label='${label}'`
