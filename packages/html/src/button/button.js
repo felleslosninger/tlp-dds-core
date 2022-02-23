136b4077a5566ba3cbf23177a40c55e1da52b4ee
Bendik Kvernevik <bendiksem93@gmail.com>
2022-02-23 18:10:24 +0100

    refactor(button): use inline SVG from import

--- packages/html/src/button/button.js
+++ packages/html/src/button/button.js
@@ -1,5 +1,6 @@
 import classnames from 'classnames'
 import { sanitizeStrict, formatHtml } from '../utils.js'
+import imageIcon from '@digdir/ds-icons/svg/image/outline.svg'
 
 const createButton = ({
   label = 'Click me',
@@ -8,8 +9,6 @@ const createButton = ({
   size = 'medium',
   iconLeft = false,
   iconRight = false,
-  iconSvgPathLeft = 'M19.51 3.08 3.08 19.51c.09.34.27.65.51.9.25.24.56.42.9.51L20.93 4.49c-.19-.69-.73-1.23-1.42-1.41zM11.88 3 3 11.88v2.83L14.71 3h-2.83zM5 3c-1.1 0-2 .9-2 2v2l4-4H5zm14 18c.55 0 1.05-.22 1.41-.59.37-.36.59-.86.59-1.41v-2l-4 4h2zm-9.71 0h2.83L21 12.12V9.29L9.29 21z',
-  iconSvgPathRight = 'M19.51 3.08 3.08 19.51c.09.34.27.65.51.9.25.24.56.42.9.51L20.93 4.49c-.19-.69-.73-1.23-1.42-1.41zM11.88 3 3 11.88v2.83L14.71 3h-2.83zM5 3c-1.1 0-2 .9-2 2v2l4-4H5zm14 18c.55 0 1.05-.22 1.41-.59.37-.36.59-.86.59-1.41v-2l-4 4h2zm-9.71 0h2.83L21 12.12V9.29L9.29 21z',
 }) => {
   label = sanitizeStrict(label)
   const classPrefix = 'dds-button'
@@ -24,21 +23,6 @@ const createButton = ({
     { [`${classPrefix}--icon-left`]: iconLeft },
     { [`${classPrefix}--icon-right`]: iconRight },
   )
-  const iconSvgLeft = String.raw`<svg
-    focusable="false"
-    viewBox="0 0 24 24"
-    aria-hidden="true"
-    >
-      <path d='${iconSvgPathLeft}'></path>
-    </svg>`
-
-  const iconSvgRight = String.raw`<svg
-    focusable="false"
-    viewBox="0 0 24 24"
-    aria-hidden="true"
-    >
-    <path d='${iconSvgPathRight}'></path>
-    </svg>`
 
   return formatHtml(String.raw`
   <button
@@ -46,9 +30,9 @@ const createButton = ({
     class='${buttonClasses}'
     ${disabled ? 'aria-disabled="true"' : ''}
   >
-    ${iconLeft ? iconSvgLeft : ''}
+    ${iconLeft ? imageIcon : ''}
     ${label}
-    ${iconRight ? iconSvgRight : ''}
+    ${iconRight ? imageIcon : ''}
   </button>`)
 }
 
