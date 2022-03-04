fccfee716d2df7a54ec424291786de92a2ac85cf
Bendik Kvernevik <bendiksem93@gmail.com>
2022-03-04 17:46:38 +0100

    fix(icon-link): rewrite to use underline and non-absolute positioned icon to support use in more contexts
    * Increase responsivity and usability by rewriting icon-link styling to use underline instead of border bottom to work better with multi-line text in flex and grid containers.
    * Use non-absolute positioned icon for cleaner implementation; to remove large amounts of code responsible for adapting to correct focus-outline.

--- packages/html/src/iconLink/iconLink.js
+++ packages/html/src/iconLink/iconLink.js
@@ -18,39 +18,42 @@ const createIconLink = ({
     [`${classPrefix}--icon-right`]: !iconLeft,
   })
 
-  const svgPathArrowForward =
-    'm12 4-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z'
-  const svgPathOpenInNew =
-    'M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z'
-  const svgPathChevronRight =
-    'M10 6L8.59 7.41L13.17 12l-4.58 4.59L10 18l6-6l-6-6z'
+  const croppedChevronSvg = String.raw`<svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      viewBox="8.59 6 7.41 12"
+      >
+      <path d="M10 6L8.59 7.41L13.17 12l-4.58 4.59L10 18l6-6l-6-6z"/>
+    </svg>`
+  const croppedOpenInNewSvg = String.raw`<svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      viewBox="3 3 18 18"
+      >
+      <path d="M19 19H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83l1.41 1.41L19 6.41V10h2V3h-7z"/>
+    </svg>`
+  const croppedArrowForwardSvg = String.raw`<svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      viewBox="4 4 16 16"
+      >
+      <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8l-8-8z"/>
+    </svg>`
 
-  let iconSvgPath = svgPathArrowForward
+  let iconSvg = croppedArrowForwardSvg
   let targetAttribute = ''
 
   if (variant === 'external') {
-    iconSvgPath = svgPathOpenInNew
+    iconSvg = croppedOpenInNewSvg
     targetAttribute = "target='_blank'"
   } else if (variant === 'shortcut') {
-    iconSvgPath = svgPathChevronRight
+    iconSvg = croppedChevronSvg
   }
 
-  const iconSvg = String.raw`
-    <svg
-      focusable="false"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      aria-hidden="true"
-      >
-      <path d="${iconSvgPath}"></path>
-    </svg>`
+  if (iconLeft) {
+    return formatHtml(String.raw`
+    <a class='${linkClasses}' href='${href}' ${targetAttribute} rel='noreferrer noopener'>${iconSvg}${label}</a>`)
+  }
 
   return formatHtml(String.raw`
-  <a class='${linkClasses}' href='${href}' ${targetAttribute} rel='noreferrer noopener'>
-    ${label}
-    ${iconSvg}
-  </a>`)
+    <a class='${linkClasses}' href='${href}' ${targetAttribute} rel='noreferrer noopener'>${label}${iconSvg}</a>`)
 }
 
 export { createIconLink }
