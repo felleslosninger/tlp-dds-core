6056d7ac375a45d8c02c744f1548ea018fac370d
Øyvind Thune <oyvlei@gmail.com>
2022-04-09 12:20:10 +0200

    fixed formatting errors

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,52 +5,48 @@ import '../packages/css/src/index.scss'
 import './customStyling.scss'
 
 export const parameters = {
-    docs: {
-        theme: customTheme,
-    },
-    backgrounds: {
-        values: [
-            {
-                name: 'grey-100',
-                value: tokens.color.neutral.grey['100'],
-            },
-            {
-                name: 'grey-200',
-                value: tokens.color.neutral.grey['200'],
-            },
+  docs: {
+    theme: customTheme,
+  },
+  backgrounds: {
+    values: [
+      {
+        name: 'grey-100',
+        value: tokens.color.neutral.grey['100'],
+      },
+      {
+        name: 'grey-200',
+        value: tokens.color.neutral.grey['200'],
+      },
+    ],
+  },
+  options: {
+    storySort: {
+      order: [
+        'Introduksjon',
+        'Tokens',
+        'Typografi',
+        'Komponenter',
+        [
+          'Kom i gang',
+          'Button',
+          'Checkbox',
+          'Radio',
+          'Toggle',
+          'Link',
+          'Icon-link',
+          'Panel-link',
+          'Input-field',
+          'Breadcrumbs',
         ],
+        'Ikoner',
+        ['Kom i gang', 'Ikonsøk', 'Guidelines'],
+        'Changelog',
+        ['Tokens', 'Ikoner', 'Core CSS', 'Core JS'],
+      ],
     },
-    options: {
-        storySort: {
-            order: [
-                'Introduksjon',
-                'Tokens',
-                'Typografi',
-                'Komponenter',
-                [
-                    'Kom i gang',
-                    'Button',
-                    'Checkbox',
-                    'Radio',
-                    'Toggle',
-                    'Link',
-                    'Icon-link',
-                    'Panel-link',
-                    'Input-field',
-                    'Breadcrumbs',
-                ],
-                'Ikoner',
-                [
-                  "Kom i gang",
-                  "Ikonsøk",
-                  "Guidelines"
-                ],
-                'Changelog',
-                ['Tokens', 'Ikoner', 'Core CSS', 'Core JS'],
-            ],
-        },
-    },
-    actions: {
-        disabled: true,
-    },
+  },
+  actions: {
+    disabled: true,
+  },
 }
